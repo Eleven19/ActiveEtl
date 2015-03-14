f3bb90ab0531abf068818e45a7692a27596de477
Damian Reeves <damreev@gmail.com>
2015-03-14 07:34:35 -0400

    Initial

---
+++ src/ActiveEtl/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ActiveEtl")>]
+[<assembly: AssemblyProductAttribute("ActiveEtl")>]
+[<assembly: AssemblyDescriptionAttribute("An ETL framework with a slightly different approach.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/ActiveEtl.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module ActiveEtl.Tests
+
+open ActiveEtl
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
