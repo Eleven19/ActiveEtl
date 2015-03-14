8610fca0bd535d416fce23abe85b422422feaf04
Damian Reeves <damreev@gmail.com>
2015-03-14 10:04:45 -0400

    Adding Connection related types

---
+++ src/ActiveEtl/Connections.fs
@@ -0,0 +1,24 @@
+﻿namespace Eleven19.ActiveEtl
+
+type ConnectionSettings = {
+  ConnectionName:string
+  ConnectionString:string
+  ProviderName:string
+}
+
+[<AutoOpen>]
+module ConnectionManager =
+  open System
+  type ConnectionSettingsRegistry = {
+    Settings: ConnectionSettings list
+  }
+
+  type ConnectionSettingsRegistry with
+    member this.TryFind name = 
+      this.Settings |> List.tryFind (fun x -> 
+        if String.Equals(name, x.ConnectionName) then true else false
+      )
+
+  let createRegistry settings =
+    {Settings=settings}
+    
---
+++ src/ActiveEtl/DataSource.fs
@@ -0,0 +1,22 @@
+﻿namespace Eleven19.ActiveEtl
+
+module DataSource =
+  open System
+  open Eleven19.ActiveEtl
+
+  module Ado =
+    open System
+    open System.Data
+    open System.Data.Common
+    open Eleven19.ActiveEtl
+
+    let CreateDbConnection (connectionSettings : ConnectionSettings) =
+      let factory = DbProviderFactories.GetFactory(connectionSettings.ProviderName)      
+      let connection = factory.CreateConnection()
+      connection.ConnectionString <- connectionSettings.ConnectionString
+      connection :> IDbConnection
+
+    let TryMakeDbConnection (registry:ConnectionSettingsRegistry) name =
+      match (registry.TryFind name) with
+      | None -> None
+      | Some settings -> settings |> CreateDbConnection |> Option.Some
