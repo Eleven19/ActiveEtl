f7bdef01024b1e75b39a7e6d7f1464ce5033964e
Damian Reeves <damreev@gmail.com>
2015-03-20 17:04:56 -0400

    Consider yourself warned

---
+++ src/ActiveEtl/Activities.fs
@@ -0,0 +1,9 @@
+﻿namespace Eleven19.ActiveEtl
+
+module Activities =
+  let createActivity impl = //context:ActivityContext -> Async<Result<ActivityContext, string>>
+    {new Activity with
+       member this.Run ctx = ctx |> impl}
+
+  let executeSql connection sql = ()  
+
